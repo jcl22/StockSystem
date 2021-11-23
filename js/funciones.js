78e7da685aeba73811b28c2e2b2fb44bbf01f62f
Jhordan_Villamil <jhordanvilla1000@gmail.com>
2021-11-23 12:32:35 -0500

    Cambios en módulo usuarios

---
+++ js/funciones.js
@@ -0,0 +1,38 @@
+// -----------------------------------------BUSCAR USUARIO---------------------------
+
+function buscarUsuario () {
+    document.getElementById ('search').style.display = 'flex';
+}
+
+// -----------------------------------------ELIMINAR USUARIO---------------------------
+// asignar 
+function mostrarAsigned() {
+    document.getElementById ('asigned').style.display = 'flex';
+}
+
+function cancelAsigned () {
+    document.getElementById ('asigned').style.display = 'none';
+}
+
+// bloquear 
+
+function mostrarBlock() {
+    document.getElementById ('block').style.display = 'flex';
+}
+
+function cancelBlock() {
+    document.getElementById ('block').style.display = 'none';
+}
+
+// eliminar 
+
+function mostrarRemove() {
+    document.getElementById ('remove').style.display = 'flex';
+}
+
+function cancelRemove() {
+    document.getElementById ('remove').style.display = 'none';
+}
+
+
+
