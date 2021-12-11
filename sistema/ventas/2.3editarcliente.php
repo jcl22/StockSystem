e9dce3423766a99c333efdee3e32ad24816f0b51
Jhordan10 <jhordanvilla1000@gmail.com>
2021-12-11 16:10:48 -0500

    CRUD clientes completo

--- sistema/ventas/2.3editarcliente.php
+++ sistema/ventas/2.3editarcliente.php
@@ -3,10 +3,9 @@ include "../../php/conexion.php";
 
 if (!empty($_POST)) {
     $alert = '';
-    if (
-        empty($_POST['id_cliente']) || empty($_POST['nombre_cliente']) 
-        || empty($_POST['direccion']) || empty($_POST['telefono'])
-    ) {
+    if (empty($_POST['id_cliente']) || empty($_POST['nombre_cliente'])
+        || empty($_POST['direccion']) || empty($_POST['telefono'])) 
+        {
         $alert = ' <h4> 
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -15,43 +14,75 @@ if (!empty($_POST)) {
                     </h4> ';
     } else {
 
-
-        $id_cliente = $_POST['id_cliente'];
+        $id_Cliente = $_POST['id_cliente']; 
         $nombre_cliente = $_POST['nombre_cliente'];
         $direccion = $_POST['direccion'];
-        $telefono = $_POST['telefono'];
+        $telefono = ($_POST['telefono']);
+        $estado = 1;
 
-        $query = mysqli_query($conn, "SELECT * FROM cliente 
-        WHERE  id_cliente = '$id_cliente' OR nombre_cliente = '$nombre_cliente'");
+        $query = mysqli_query($conn, "SELECT * FROM cliente
+         WHERE  (nombre_cliente = '$nombre_cliente' AND id_cliente != $id_Cliente) ");
+        
         $result = mysqli_fetch_array($query);
+
 
         if ($result > 0) {
             $alert = '  <h6> 
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
                                 <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
                                 <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
-                            </svg> <b> Error! El cliente ya se encuentra registrado. </b> 
+                            </svg> <b> Error! El cliente ya existe. </b> 
                         </h6> ';
         } else {
-            $query_insert = mysqli_query($conn, "INSERT INTO cliente (id_cliente, nombre_cliente, 
-            direccion, telefono) VALUES ('$id_cliente','$nombre_cliente','$direccion', '$telefono')");
 
 
-            if ($query_insert) {
+
+                $sql_update = mysqli_query ($conn, "UPDATE cliente SET nombre_cliente = '$nombre_cliente', 
+                direccion = '$direccion', telefono = '$telefono'
+                WHERE id_cliente = $id_Cliente");
+
+
+            if ($sql_update) {
                 $alert = '  <h5> 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                     <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-                                </svg> <b> El cliente se ha creado correctamente. </b> 
+                                </svg> <b> El cliente se ha actualizado correctamente. </b> 
                             </h5> ';
             } else {
                 $alert = ' <h4 > 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                </svg> <b> Error al crear cliente.</b> </h4> ';
+                                </svg> <b> Error al actualizar el cliente. </b> </h4> ';
+
             }
         }
+    }
+}
+
+// mostrar datos
+
+if (empty($_GET['id'])) {
+    header ('location:2.2listaclientes.php');
+}
+$id_Cliente =  $_GET['id'];
+
+$sql= mysqli_query($conn, "SELECT * FROM cliente WHERE id_cliente=$id_Cliente");
+
+$result_edit = mysqli_num_rows($sql);
+
+if ($result_edit == 0 ) {
+    header ('location:2.2listacliente.php');
+} else {
+
+    
+    while ($data = mysqli_fetch_array($sql)) {
+        $id_Cliente = $data ['id_cliente'];
+        $nombre_cliente = $data ['nombre_cliente'];
+        $direccion = $data ['direccion'];
+        $telefono = $data ['telefono'];
+        $estado = 1;
     }
 }
 
@@ -64,7 +95,7 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Ventas | Clientes / Crear </title>
+    <title>Ventas | Clientes / Editar</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
 
@@ -87,9 +118,9 @@ if (!empty($_POST)) {
 
     <section id="content-section" >
         <div class="titulo-section">
-            <svg class="img-agguser" xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
-                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+            <svg class="img-editproduct" xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg>
         </div>
         <div class="container-form">
@@ -99,29 +130,27 @@ if (!empty($_POST)) {
                     <form action="" method="post">
                         <div class="form-content ">
                             <div class="col-sm-4-left">
+                                    <input type="hidden" name="id_cliente" id="" value=" <?php echo $id_Cliente; ?> ">
                                 <div class="form-group">
-                                    <input type="text" name="id_cliente" id="" placeholder="ID cliente">
-                                </div> 
-                                <div class="form-group">
-                                    <input type="text" name="nombre_cliente" id="" placeholder="Nombre">
-                                </div> 
+                                    <input type="text" name="nombre_cliente" id="" placeholder="nombre_cliente" value= "<?php echo $nombre_cliente; ?>" >
+                                </div>
+ 
                             </div>
 
                             <div class="col-sm-4-right">
                                 <div class="form-group">
-                                    <input type="text" name="direccion" id="" placeholder="Dirección">
+                                    <input type="text" name="direccion" id="" placeholder="Dirección" value= "<?php echo $direccion; ?>" >
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" name="telefono" id="" placeholder="Teléfono">
-                                </div> 
+                                    <input type="text" name="telefono" id="" placeholder="Teléfono" value= "<?php echo $telefono; ?>" >
+                                </div>
                             </div>
                         </div>
-
                         <div class="botones">
-                            <button id="button-crear"type="submit" class="btn btn-success">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save2" viewBox="0 0 16 16">
-                                    <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v4.5h2a.5.5 0 0 1 .354.854l-2.5 2.5a.5.5 0 0 1-.708 0l-2.5-2.5A.5.5 0 0 1 5.5 6.5h2V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
-                                </svg>Crear
+                            <button id="button-modificar" type="submit" class="btn btn-warning">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+                                    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
+                                </svg>Actualizar
                             </button>
                         </div>
                     </form>
@@ -137,7 +166,7 @@ if (!empty($_POST)) {
         <div class="content">
             <div class="botones-abl-footer">
                 <div class="float-left boton wow pulse" data-wow-iteration="infinite" data-wow-duration="500ms">
-                    <a href="2clientes.php" class="boton btn btn-primary"><i
+                    <a href="2.2listaclientes.php" class="boton btn btn-primary"><i
                             class="fas fa-chevron-circle-left"></i></a>
                 </div>
                 <div class="float-right boton wow pulse" data-wow-iteration="infinite" data-wow-duration="500ms">
--- sistema/ventas/buscadorclientes.php
+++ sistema/ventas/buscadorclientes.php
@@ -10,7 +10,7 @@ include "../../php/conexion.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Compras | Proveedores/Lista</title>
+    <title>Ventas | Clientes / Lista</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
 
@@ -41,9 +41,16 @@ include "../../php/conexion.php";
         
 
         <div class="content-crear">
+            <div class="content-crear">
+            <?php 
+                $busqueda = strtolower ($_REQUEST['busqueda']);
+                if (empty($busqueda)) {
+                header('location:2.2listaclientes.php');
+                }
+            ?> 
             
         <!-- busqueda -->
-        <form class="input-group rounded" action="buscadorproveedores.php" method="get">
+        <form class="input-group rounded" action="buscadorclientes.php" method="get">
             <input type="text" class="form-control rounded"  aria-label="Search" aria-describedby="search-addon" name="busqueda" id="busqueda" placeholder="Buscar" />
             <button  class="input-group-text border-0" id="search-addon">
                 <i type="submit" class="fas fa-search"></i>
@@ -51,10 +58,10 @@ include "../../php/conexion.php";
         </form>
             <table>
                 <tr>
-                    <th>ID proveedor</th>
+                    <th>ID cliente</th>
                     <th>Nombre</th>
                     <th>Dirección</th>
-                    <th>Teléfono</th>
+                    <th>Costo Teléfono</th>
                     <?php
                     if ($tipo_rol == 'Admin') {
                     ?>
@@ -66,7 +73,14 @@ include "../../php/conexion.php";
                 //paginador
 
                 $sql_registe = mysqli_query($conn, "SELECT COUNT(*) as total_registros 
-                FROM proveedor WHERE estado = 1");
+                FROM cliente WHERE 
+                (id_cliente LIKE '%$busqueda%' OR
+                nombre_cliente LIKE '%$busqueda%' OR
+                direccion LIKE '%$busqueda%' OR
+                telefono LIKE '%$busqueda%')
+                AND
+                
+                estado = 1");
 
                 $result_register = mysqli_fetch_array($sql_registe);
                 $total_registro = $result_register['total_registros'];
@@ -81,10 +95,17 @@ include "../../php/conexion.php";
 
                 $desde = ($pagina - 1) * $por_pagina;
                 $total_paginas = ceil($total_registro / $por_pagina);
+               
 
+                $query = mysqli_query($conn, "SELECT * FROM cliente
+                WHERE
+                (id_cliente LIKE '%$busqueda%' OR
+                nombre_cliente LIKE '%$busqueda%' OR
+                direccion LIKE '%$busqueda%' OR
+                telefono LIKE '%$busqueda%' )
+                AND
+                estado = 1 ORDER BY id_cliente ASC LIMIT $desde, $por_pagina");
 
-                $query = mysqli_query($conn, "SELECT * FROM proveedor WHERE estado=1 ORDER BY id_proveedor 
-                ASC");
 
                 $result = mysqli_num_rows($query);
 
@@ -93,8 +114,8 @@ include "../../php/conexion.php";
 
                 ?>
                     <tr class="datos-usuario">
-                        <td> <?php echo $data["id_proveedor"] ?> </td>
-                        <td> <?php echo $data["nombre_proveedor"] ?> </td>
+                        <td> <?php echo $data["id_cliente"] ?> </td>
+                        <td> <?php echo $data["nombre_cliente"] ?> </td>
                         <td> <?php echo $data["direccion"] ?> </td>
                         <td> <?php echo $data["telefono"] ?> </td>
 
@@ -105,7 +126,7 @@ include "../../php/conexion.php";
                             ?>
 
                                 <td class="buttons">
-                                        <a href="2.3editarproveedor.php?id=<?php echo $data["id_proveedor"] ?>">
+                                        <a href="2.3editarcliente.php?id=<?php echo $data["id_cliente"] ?>">
                                             <button type="button" id="button-editar" class="btn btn-warning">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -115,7 +136,7 @@ include "../../php/conexion.php";
                                             </button>
                                         </a>
 
-                                        <a href="2.4eliminarproveedor.php?id=<?php echo $data["id_proveedor"] ?>">
+                                        <a href="2.4eliminarcliente.php?id=<?php echo $data["id_cliente"] ?>">
                                             <button type="button" id="button-eliminar" class="btn btn-danger">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
@@ -133,14 +154,17 @@ include "../../php/conexion.php";
 
 
             </table>
+            <?php
+            if($total_registro != 0) {
+            ?>
             <div class="paginador">
                 <ul>
                     <?php
                     if ($pagina != 1) {
                     ?>
-                        <li><a href="?pagina= <?php echo 1; ?> "> |<< </a>
+                        <li><a href="?pagina= <?php echo 1; ?>&busqueda=<?php echo $busqueda;?>"> |<< </a>
                         </li>
-                        <li><a href="?pagina= <?php echo $pagina - 1; ?>">
+                        <li><a href="?pagina= <?php echo $pagina - 1; ?>&busqueda=<?php echo $busqueda;?>">
                                 << </a>
                         </li>
                     <?php
@@ -149,27 +173,28 @@ include "../../php/conexion.php";
 
 
                         if ($i == $pagina) {
-                            echo '<li class="pageSelected">' . $i . '</li>';
+                            echo '<li class="pageSelected">'.$i.'</li>';
                         } else {
-                            echo '<li><a href="?pagina=' . $i . '">' . $i . '</a></li>';
+                            echo '<li><a href="?pagina='.$i.'&busqueda='.$busqueda.'">'.$i.'</a></li>';
                         }
                     }
                     if ($pagina != $total_paginas) {
                     ?>
 
 
-                        <li><a href="?pagina= <?php echo $pagina + 1; ?>"> >> </a></li>
-                        <li><a href="?pagina= <?php echo $total_paginas; ?> "> >>| </a></li>
+                        <li><a href="?pagina= <?php echo $pagina + 1; ?>&busqueda=<?php echo $busqueda; ?>"> >> </a></li>
+                        <li><a href="?pagina= <?php echo $total_paginas; ?>&busqueda=<?php echo $busqueda; ?>"> >>| </a></li>
                     <?php } ?>
                 </ul>
             </div>
+            <?php } ?>
         </div>
 
         <section id="botones-footer">
             <div class="content">
                 <div class="botones-abl-footer">
                     <div class="float-left boton wow pulse" data-wow-iteration="infinite" data-wow-duration="500ms">
-                        <a href="2proveedores.php" class="boton btn btn-primary"><i class="fas fa-chevron-circle-left"></i></a>
+                        <a href="2clientes.php" class="boton btn btn-primary"><i class="fas fa-chevron-circle-left"></i></a>
                     </div>
                     <div class="float-right boton wow pulse" data-wow-iteration="infinite" data-wow-duration="500ms">
                         <a href="../index.php" class="boton btn btn-primary"><i class="fas fa-home"></i></a>
